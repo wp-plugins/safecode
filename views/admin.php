e60c7725c270fec87ad9cb7f9cf1f36905f8f9e6
shazdeh <shazdeh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-27 22:37:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/safecode/trunk@604913 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/admin.php
@@ -0,0 +1,26 @@
+<?php
+
+defined( 'ABSPATH' ) or die( '-1' );
+?>
+<div class="wrap">
+	<?php screen_icon() ?>
+	<h2>SafeCode</h2><br />
+
+	<?php if( isset( $_POST['updated'] ) && 1 == (int) $_POST['updated'] ) : ?>
+		<p><?php _e( 'Settings saved.' ); ?></p>
+	<?php endif; ?>
+
+	<form action="" method="post" id="custom-functions">
+		<textarea cols="70" rows="25" style="font-family: Consolas,Monaco,monospace; font-size: 12px; width: 100%; background: #F9F9F9; outline: none; padding: 5px;" name="custom-functions" id="newcontent" dir="ltr" tabindex="1"><?php echo esc_html( get_option( 'safecode', "<?php \n\n" ) ) ?></textarea>
+		<input type="hidden" name="scrollto" id="scrollto" value="<?php echo isset( $_REQUEST['scrollto'] ) ? (int) $_REQUEST['scrollto'] : 0; ?>" />
+		<?php submit_button() ?>
+	</form>
+</div><!-- .wrap -->
+<script type="text/javascript">
+/* <![CDATA[ */
+jQuery(document).ready(function($){
+	$('#custom-functions').submit(function(){ $('#scrollto').val( $('#newcontent').scrollTop() ); });
+	$('#newcontent').scrollTop( $('#scrollto').val() );
+});
+/* ]]> */
+</script>
